92bfb4cfb1bf3f534c5708c5649668eb926aa3df
Sven <sven@mutzl.org>
2018-03-30 12:22:34 +0200

    Remove unused mysql function

--- models/transaction.js
+++ models/transaction.js
@@ -8,7 +8,6 @@ var mongo = require('../libraries/mongo.js');
 
 
 module.exports = {
-    listTxsByAddress: listTxsByAddress,
     fetch: fetch,
     locksum: locksum,
     circulation: circulation,
@@ -28,45 +27,13 @@ function fetch(hash) {
                     "hash": hash
                 }).toArray((err, docs) => {
                     if (err || docs.length !== 1) {
-                        console.error(err, number);
+                        console.error(err);
                         throw Error("ERROR_FETCH_TX");
                     } else {
                         resolve(docs[0]);
                     }
                 });
             });
-    });
-}
-
-
-/**
- * List transactions of given address
- * @param {} address
- * @returns {}
- */
-function listTxsByAddress(address, page, num) {
-    return new Promise((resolve, reject) => {
-        var sql = "SELECT SQL_CALC_FOUND_ROWS `tx`.`id`,`tx`.`block_height`,`tx`.`hash` FROM `tx` JOIN `tx_input` ON `tx`.`id`=`tx_input`.`tx_id` JOIN `address` as `input_address` ON `tx_input`.`address_id`=`input_address`.`id` AND `input_address`.`address`=? JOIN `tx_output` ON `tx_output`.`tx_id`=`tx`.`id` JOIN `address` as `output_address` ON `tx_output`.`address_id`=`output_address`.`id` AND `output_address`.`address`=?  GROUP BY `tx`.`id` ORDER BY `tx`.`block_height` DESC LIMIT ?,?";
-        connection.query(sql, [address, address, page * num, num], (error, result) => {
-            if (error) {
-                console.log(error);
-                reject(Error("ERR_FETCH_TRANSACTIONS"));
-            } else {
-                connection.query('SELECT FOUND_ROWS() as count;', (err, res) => {
-                    if (error) {
-                        console.log(error);
-                        reject(Error("ERR_FETCH_TRANSACTIONS"));
-                    } else {
-                        resolve({
-                            transactions: result,
-                            count: res[0].count,
-                            items_per_page: num
-                        });
-                    }
-                });
-            }
-
-        });
     });
 }
 
