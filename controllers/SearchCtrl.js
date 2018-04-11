c4b3155fd29123f3c323b5a4f4b2820a9dd64164
Sven <sven@mutzl.org>
2018-04-11 10:03:13 +0200

    Error handling and restriction for suggestions

--- controllers/SearchCtrl.js
+++ controllers/SearchCtrl.js
@@ -6,6 +6,7 @@ var Transaction = require('../models/transaction');
 var Block = require('../models/block');
 var Address = require('../models/address');
 var Asset = require('../models/assets');
+var Helper = require('../libraries/helper.js');
 
 exports.Suggest = suggest;
 
@@ -17,12 +18,17 @@ exports.Suggest = suggest;
 function suggest(req, res) {
     var prefix = req.params.prefix;
     var limit = parseInt(req.query.limit) || 10;
+
     Promise.all([
+        Helper.checkError(limit <= 20, 'ERR_LIMIT_RANGE'),
+        Helper.checkError(prefix.length>=3, 'ERR_PREFIX_LENGTH')
+    ])
+        .then(() => Promise.all([
             Transaction.suggest(prefix, limit),
             Address.suggest(prefix, limit, true),
             Block.suggest(prefix, limit),
             Asset.suggest(prefix, limit)
-        ])
+        ]))
         .then((suggestions) => {
             res.json(Message(1, undefined, {
                 tx: suggestions[0],
@@ -33,6 +39,6 @@ function suggest(req, res) {
         })
         .catch((error) => {
             console.error(error);
-            res.status(404).json(Message(0, 'ERR_SUGGESTIONS'));
+            res.status(404).json(Message(0, error.message));
         });
 }
