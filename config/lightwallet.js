2d08820cbd4c9c94a395c946e8e1bb5dd8ee94f6
Sven <sven@mutzl.org>
2018-06-25 12:24:39 +0200

    Listwallet android version definition by environment

--- config/lightwallet.js
+++ config/lightwallet.js
@@ -1,5 +1,5 @@
 module.exports = {
-    version: 302,
-    name: 'MyETPWallet',
-    url: 'https://metaverse.nyc3.digitaloceanspaces.com/lightwallet/app/mvs-lw-0.3.1r2.apk'
+    "version": (process.env.LW_ANDROID_VERSION) ? process.env.LW_ANDROID_VERSION : 302,
+    "name": (process.env.LW_ANDROID_NAME) ? process.env.LW_ANDROID_NAME : 'MyETPWallet',
+    "url": (process.env.LW_ANDROID_URL) ? process.env.LW_ANDROID_URL : 'https://metaverse.nyc3.digitaloceanspaces.com/lightwallet/app/mvs-lw-0.3.1r2.apk'
 };
