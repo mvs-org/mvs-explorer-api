9743693c4d8d2a88147e052bbe0af9eb78f54fda
Sven <sven@mutzl.org>
2018-09-03 11:02:49 +0200

    Bridge config

---
+++ models/bridge.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Set up database
+var mongo = require('../libraries/mongo.js');
+
+
+module.exports = {
+    list: listMST,
+    config: config
+};
+
+/**
+ * Get the list of all the compatible MST configs.
+ * @param {} hash
+ * @returns {}
+ */
+function config() {
+    return mongo.connect()
+        .then((db) => db.collection('bridge'))
+        .then(collection => collection.findAll({}, {
+            "_id": 0
+        }))
+        .then(cursor => cursor.toArray());
+}
+
+function listMST() {
+    return config()
+        .then((list) => list.map(item=>item.MST));
+}
