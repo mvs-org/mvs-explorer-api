f49da918090f69ac6102e4d1a2b33da94368aa76
Laurent Salou <laurent.salou@hotmail.fr>
2019-09-18 18:19:34 +0800

    Add DNA in pricing/tickers call

--- models/coingecko.js
+++ models/coingecko.js
@@ -1,4 +1,5 @@
 let requestify = require('requestify');
+let dnaRightBtcResponse = {}
 
 module.exports = {
     tickers: tickers
@@ -15,12 +16,28 @@ let bases = [
 ];
 
 function tickers() {
-    return requestify.get(`https://api.coingecko.com/api/v3/simple/price?ids=metaverse-etp%2Czengold&vs_currencies=${bases.join('%2C')}`)
+    return requestify.get(`https://api.coingecko.com/api/v3/simple/price?ids=metaverse-etp%2Czengold%2Cbitcoin&vs_currencies=${bases.join('%2C')}`)
         .then(result => result.getBody())
         .then(result => {
             const res = { ETP: {}, "MVS.ZGC": {}, };
             Object.entries(result['metaverse-etp']).forEach(([currency, price]) => res['ETP'][currency.toUpperCase()] = { price, })
             Object.entries(result.zengold).forEach(([currency, price]) => res['MVS.ZGC'][currency.toUpperCase()] = { price, })
-            return res
+            return requestify.get(`https://api.rightbtc.pro/v1/pub/ticker/DNAUSDT`)
+                .then(dna => dna.getBody())
+                .then(dna => {
+                    dnaRightBtcResponse = dna
+                    res.DNA = {}
+                    Object.entries(result.bitcoin).forEach(([currency, price]) => res['DNA'][currency.toUpperCase()] = { price: price/result.bitcoin.usd*dna.ticks/1000000, })
+                    return res
+                })
+                .catch((error) => {
+                    console.error(error)
+                    if(dnaRightBtcResponse.ticks) {
+                        res.DNA = {}
+                        Object.entries(result.bitcoin).forEach(([currency, price]) => res['DNA'][currency.toUpperCase()] = { price: price/result.bitcoin.usd*dnaRightBtcResponse.ticks/1000000, })
+                    }
+                    return res
+                });
+            
         })
 }
